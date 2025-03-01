90e5434a9887748190db254d65414e6bbc3b3e2c
OstrykovBogdan <ostrykovbogda@gmail.com>
2025-03-01 20:05:34 +0200

    minor changes

--- app/config.py
+++ app/config.py
@@ -3,4 +3,6 @@ import os
 class Config:
     FLASK_DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    if not SQLALCHEMY_DATABASE_URI:
+        SQLALCHEMY_DATABASE_URI="postgresql://Bohdan23:app_pass@db:5432/flask_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
