84c50cf33bdcfa7fc890776ce04a1d2a386ef50b
OstrykovBogdan <ostrykovbogda@gmail.com>
2025-03-02 20:20:33 +0200

    Changed DATABASE_URL variable

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     FLASK_DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     if not SQLALCHEMY_DATABASE_URI:
         SQLALCHEMY_DATABASE_URI="postgresql://Bohdan23:app_pass@db:5432/flask_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
